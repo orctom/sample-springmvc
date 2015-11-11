af044a7be82719326aa849687289f561f735d952
Hao <orctom@gmail.com>
2015-11-11 19:11:58 +0800

    sample

--- src/test/java/com/orctom/sample/Test.java
+++ src/test/java/com/orctom/sample/Test.java
@@ -11,21 +11,5 @@ public class Test {
 		String s1 = "/products/{id}/attributes/{attid}";
 		String s2 = "/products/234235234/attributes/222222";
 		// Assert.assertEquals(s1, s2);
-
-		String template = "jython/was.py";
-		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(template);
-		System.out.println("input = " + input);
-
-		InputStreamReader isr = new InputStreamReader(input);
-		BufferedReader br = new BufferedReader(isr);
-		String line;
-		System.out.println("==========================");
-		while ((line = br.readLine()) != null) {
-			System.out.println(">> " + line);
-		}
-		System.out.println("==========================");
-		br.close();
-		isr.close();
-		input.close();
 	}
 }
