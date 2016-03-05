9c5fc8b2e90b988907cefcc02b5ee055264e573d
Hao <orctom@gmail.com>
2016-03-05 09:30:08 +0800

    sample

--- src/main/java/com/orctom/sample/springmvc/controller/IndexController.java
+++ src/main/java/com/orctom/sample/springmvc/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.orctom.sample.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,6 +15,12 @@ public class IndexController {
 
 	@RequestMapping(value = "/sample", method = RequestMethod.GET)
 	public Iterable<String> test() {
+		return ImmutableList.<String> builder().add("hello").add("would").add("spring-mvc").build();
+	}
+
+	@RequestMapping(value = "/sample2/{name}", method = RequestMethod.GET)
+	public Iterable<String> test2(@PathVariable String name) {
+		System.out.println(name);
 		return ImmutableList.<String> builder().add("hello").add("would").add("spring-mvc").build();
 	}
 }
